93defca8e2f9b14344bbd59fdcd94ce8cc905109
mainak-biswas1999 <81000840+mainak-biswas1999@users.noreply.github.com>
2022-03-13 23:55:28 +0530

    Update ray.py

--- src/utils/ray.py
+++ src/utils/ray.py
@@ -3,7 +3,7 @@ import socket
 
 def get_local_ray_dir():
     hname = socket.gethostname()
-
-    raise NotImplementedError, 'You must specify the local directory for ray to store results'
+    #this has been changed
+    raise NotImplementedError('You must specify the local directory for ray to store results')
     local_dir += 'ray_results'
     return local_dir
